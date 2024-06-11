5a0a0ff1142f8c1ea635220578d30222e45cf2f0
Arnaud Van Acker <arnaudvanacker@yahoo.fr>
2024-06-11 12:14:01 +0200

    bientot miam

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,48 @@
 import { sendForm } from "./request.js"
 
 //init
-const inputName = document.querySelector('#nameID');
-const surname = document.querySelector('#surnameID');
-const gender = document.querySelector('#genderID');
-const email = document.querySelector('#emailID');
-const country = document.querySelector('#countryID');
-const subject = document.querySelector('#subjectID');
-const message = document.querySelector('#messageID');
+const honeypot = document.querySelector('#honeypotID')
+honeypot.style.display = 'none'
 
-const form = [inputName, surname, gender, email, country, subject, message];
+const inputName = document.querySelector('#nameID')
+const surname = document.querySelector('#surnameID')
+const gender = document.querySelector('#genderID')
+const email = document.querySelector('#emailID')
+const country = document.querySelector('#countryID')
+const subject = document.querySelector('#subjectID')
+const message = document.querySelector('#messageID')
+
+const form = [inputName, surname, gender, email, country, subject, message]
 
 form.forEach(element => {
-    element.value = ''
+    switch (element) {
+        case gender:
+            element.value = 'none'
+            break
+
+        case subject:
+            element.value = 'other'
+            break
+
+        default:
+            element.value = ''
+    }
 });
 
 //send form
 document.querySelector('#sendButton').addEventListener('click', () => {
-    sendForm()
+    if(honeypot.value != '') {
+        console.log('You fool')
+    } else {
+        sendForm() 
+    }
+})
+
+//socials
+document.querySelector('#github').addEventListener('click', () => {
+    open('https://github.com/javadaller','_blank')
+})
+
+document.querySelector('#linkedin').addEventListener('click', () => {
+    open('https://www.linkedin.com/in/arnaud-van-acker','_blank')
 })
